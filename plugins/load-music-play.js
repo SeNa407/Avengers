5855e6666020a7248520b314652c305cd7e5d15d
SeNa407 <67375996+SeNa407@users.noreply.github.com>
2020-12-26 23:45:21 +0100

    load-music-play.js was changed

--- plugins/load-music-play.js
+++ plugins/load-music-play.js
@@ -1,17 +1,14 @@
 var count = 0;
-var buttonPlayPause = document.getElementsByClassName(
-  "btn btn-header btn-play"
-);
-var innere = buttonPlayPause.textContent;
+var buttonPlayPause = document.querySelector(".btn-play");
 
 let play = function () {
   if (count == 0) {
     count = 1;
     document.getElementById("audio").play();
-    innere = "&#10074 &#10074";
+    buttonPlayPause.innerHTML = "&#10074 &#10074";
   } else {
     count = 0;
     document.getElementById("audio").pause();
-    innere = "&#10074 &#10074";
+    buttonPlayPause.innerHTML = "&#9658";
   }
 };
